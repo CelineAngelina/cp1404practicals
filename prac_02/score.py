2d4ae043cde29b2df75a55213e7e2d89955d79f6
CA <angelina.celine@my.jcu.edu.au>
2025-05-28 19:09:22 +0800

    Added function that generate random score

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,8 +1,15 @@
+import random
+
 def main():
     """Get user score and print it result."""
-    score = float(input("Enter score: "))
+    # score = float(input("Enter score: "))
+    score = generate_random_score()
     result = determine_score(score)
-    print(result)
+    print(f"Your score - {score} is {result}")
+
+def generate_random_score():
+    """Generate a random score that is in between 0-100."""
+    return random.randint(0,100)
 
 def determine_score(score):
     """Determine user's result based on the score."""
