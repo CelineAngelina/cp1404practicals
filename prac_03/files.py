908a8c6b6e11f1aeca79db34cc0896a6b84e5073
CA <angelina.celine@my.jcu.edu.au>
2025-06-04 23:39:04 +0800

    Add code to extract numbers from file and add them together.

--- prac_03/files.py
+++ prac_03/files.py
@@ -6,8 +6,17 @@
 # output_file.close()
 
 # 2.
-FILENAME = "name.txt"
-output_file = open(FILENAME, 'r')
-name = output_file.read().strip()
-print(f"Hi {name}!")
-output_file.close()
+# FILENAME = "name.txt"
+# output_file = open(FILENAME, 'r')
+# name = output_file.read().strip()
+# print(f"Hi {name}!")
+# output_file.close()
+
+# 3.
+FILENAME = "numbers.txt"
+with open(FILENAME, 'r') as out_file:
+    first_number= int(out_file.readline().strip())
+    second_number = int(out_file.readline().strip())
+    total = first_number + second_number
+    print(total)
+
