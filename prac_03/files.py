56e7463eba4faf90b934002523a0cb9e2bf298d1
CA <angelina.celine@my.jcu.edu.au>
2025-06-04 23:55:19 +0800

    Add code to read numbers from file, sum them, and append total to a new line.

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,16 +1,16 @@
 # 1.
-# FILENAME = "name.txt"
-# output_file = open(FILENAME, "w")
-# name = input("Enter your name: ")
-# print(name, file=output_file)
-# output_file.close()
+FILENAME = "name.txt"
+output_file = open(FILENAME, "w")
+name = input("Enter your name: ")
+print(name, file=output_file)
+output_file.close()
 
 # 2.
-# FILENAME = "name.txt"
-# output_file = open(FILENAME, 'r')
-# name = output_file.read().strip()
-# print(f"Hi {name}!")
-# output_file.close()
+FILENAME = "name.txt"
+output_file = open(FILENAME, 'r')
+name = output_file.read().strip()
+print(f"Hi {name}!")
+output_file.close()
 
 # 3.
 FILENAME = "numbers.txt"
@@ -20,3 +20,12 @@ with open(FILENAME, 'r') as out_file:
     total = first_number + second_number
     print(total)
 
+# 4.
+FILENAME = "numbers.txt"
+total = 0
+with open(FILENAME, 'r') as in_file:
+    for line in in_file:
+        number = int(line.strip())
+        total += number
+with open(FILENAME, 'a') as out_file:
+    out_file.write(f"{total}")
