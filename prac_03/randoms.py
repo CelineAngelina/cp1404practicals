1bbcbeef07a9357f22ed09d9fe1a1b319a6da792
CA <angelina.celine@my.jcu.edu.au>
2025-06-04 16:37:39 +0800

    first try on the random codes

---
+++ prac_03/randoms.py
@@ -0,0 +1,18 @@
+import random
+
+print(random.randint(5, 20))  # 14
+print(random.randrange(3, 10, 2))  # 3
+print(random.uniform(2.5, 5.5))  # 4.392172220792992
+
+# What did you see on line 1?
+# on the first try, the output was 14
+# What was the smallest number you could have seen, what was the largest?
+#
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# Could line 2 have produced a 4?
+#
+# What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+#
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
