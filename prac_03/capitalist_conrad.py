143e3544ee5009ae50585d0c7288e6e956ad9339
CA <angelina.celine@my.jcu.edu.au>
2025-06-04 17:24:44 +0800

    Refactor code to write output to a file.

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -5,9 +5,12 @@ MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 1
 MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
+FILENAME = "output.txt"
+
+out_file = open(FILENAME, 'w')
 
 price = INITIAL_PRICE
-print(f"Starting price ${price:,.2f}")
+print(f"Starting price ${price:,.2f}", file=out_file)
 
 number_of_days = 0
 
@@ -26,4 +29,6 @@ while MIN_PRICE <= price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print(f"On day {number_of_days} price is: ${price:,.2f}")
+    print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
+
+out_file.close()
