c17e01cd11ecd875ec02b423751d0292cf76daa3
CA <angelina.celine@my.jcu.edu.au>
2025-06-11 18:30:26 +0800

    Refactor new function to display subject details.

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -1,10 +1,10 @@
 FILENAME = "subject_data.txt"
 
-
 def main():
     data = load_data()
     print(data)
-
+    print()
+    print_subject_details(data)
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
@@ -22,5 +22,10 @@ def load_data():
             print("----------")
             data.append(parts)
     return data
+
+def print_subject_details(data):
+    """Display subject details."""
+    for i in range(len(data)):
+        print(f"{data[i][0]} is taught by {data[i][1]:12} and has {data[i][2]:3} students")
 
 main()
