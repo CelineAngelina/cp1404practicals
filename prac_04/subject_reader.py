b7e477231e8f057ce6e515dfcaeb0f3449be8b8b
CA <angelina.celine@my.jcu.edu.au>
2025-06-11 18:43:07 +0800

    Comment out unnecessary print statements in load_data().

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -1,6 +1,7 @@
 FILENAME = "subject_data.txt"
 
 def main():
+    """Load subject data and display it in raw and formatted form."""
     data = load_data()
     print(data)
     print()
@@ -9,18 +10,17 @@ def main():
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     data = []
-    with open(FILENAME) as input_file:
-        lines = input_file.readlines()
-        for line in lines:
-            print(line)  # See what a line looks like
-            print(repr(line))  # See what a line really looks like
-            line = line.strip()  # Remove the \n
-            parts = line.split(',')  # Separate the data into its parts
-            print(parts)  # See what the parts look like (notice the integer is a string)
-            parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-            print(parts)  # See if that worked
-            print("----------")
-            data.append(parts)
+    input_file = open(FILENAME)
+    for line in input_file:
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
+        line = line.strip()  # Remove the \n
+        parts = line.split(',')  # Separate the data into its parts
+        # print(parts)  # See what the parts look like (notice the integer is a string)
+        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+        # print(parts)  # See if that worked
+        # print("----------")
+        data.append(parts)
     return data
 
 def print_subject_details(data):
