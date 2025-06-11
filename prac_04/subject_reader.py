6837fcbaef161d5e532b87e24e515b38a6172db9
CA <angelina.celine@my.jcu.edu.au>
2025-06-11 17:52:19 +0800

    Amend load_data function so it will print the nested list.

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,22 +8,19 @@ def main():
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
-    input_file = open(FILENAME)
-    for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
-    input_file.close()
-
+    data = []
+    with open(FILENAME) as input_file:
+        lines = input_file.readlines()
+        for line in lines:
+            print(line)  # See what a line looks like
+            print(repr(line))  # See what a line really looks like
+            line = line.strip()  # Remove the \n
+            parts = line.split(',')  # Separate the data into its parts
+            print(parts)  # See what the parts look like (notice the integer is a string)
+            parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+            print(parts)  # See if that worked
+            print("----------")
+            data.append(parts)
+    return data
 
 main()
-
-CP1401,Ada Lovelace,192
-CP1404,Alan Turing,98
-CP4321,Bill Gates,676
-CP1234,Steve Jobs,123
