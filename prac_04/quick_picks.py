b1f8b8cd943eeeedb103d381ead9a909d38d77a4
CA <angelina.celine@my.jcu.edu.au>
2025-06-11 21:24:20 +0800

    Implement code to generate lines of random numbers based on user input.

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,16 @@
+import random
+
+NUMBER_PER_LINE = 6
+MINIMUM = 1
+MAXIMUM = 45
+
+quick_pick = int(input("How many quick picks? "))
+
+for i in range(quick_pick):
+    numbers = []
+    while len(numbers) < NUMBER_PER_LINE:
+        number = random.randint(MINIMUM, MAXIMUM)
+        if number not in numbers:
+            numbers.append(number)
+            numbers.sort()
+    print(" ".join((f"{number:2}" for number in numbers)))
