1e0d50d66d86c7b782bb43ccd15f8cf08c332f7a
CA <angelina.celine@my.jcu.edu.au>
2025-06-11 21:37:38 +0800

    Add error checking for quick pick negative value input.

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -5,6 +5,9 @@ MINIMUM = 1
 MAXIMUM = 45
 
 quick_pick = int(input("How many quick picks? "))
+while quick_pick < 0:
+    print("Invalid pick")
+    quick_pick = int(input("How many quick picks? "))
 
 for i in range(quick_pick):
     numbers = []
@@ -12,5 +15,5 @@ for i in range(quick_pick):
         number = random.randint(MINIMUM, MAXIMUM)
         if number not in numbers:
             numbers.append(number)
-            numbers.sort()
+        numbers.sort()
     print(" ".join((f"{number:2}" for number in numbers)))
