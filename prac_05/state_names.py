61ac52991b82ae11f3aee8caddaf07e24431c70d
CA <angelina.celine@my.jcu.edu.au>
2025-06-18 18:15:58 +0800

    Refactor output to print all the states and names neatly lined up.

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -1,12 +1,15 @@
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
 print(CODE_TO_NAME)
+states = []
 
-state_code = input("Enter short state: ")
+state_code = input("Enter short state: ").upper()
 while state_code != "":
     if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
+        states.append((state_code, CODE_TO_NAME[state_code]))
     else:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ").upper()
+print()
+for code, name in states:
+    print(f"{code:3} is {name}")
