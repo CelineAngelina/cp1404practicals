98e180b96ae1ffb51416f5aaafb25acc4afbdaf7
CA <angelina.celine@my.jcu.edu.au>
2025-06-20 02:13:03 +0800

    Refactor code by adding function to display countries of champions and added docstring for all of the functions.

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,19 +1,22 @@
 """
 Wimbledon
 Estimate: 60 minutes
-Actual:    minutes
-start 12:08
+Actual:  102 minutes
 """
 import csv
 
-def main ():
+def main():
+    """Read Wimbledon data and display champion win counts and countries."""
     filename = "wimbledon.csv"
-    champion_to_count = load_champion_count(filename)
-    display_champion_name_and_count(champion_to_count)
+    champion_to_count, countries = load_wimbledon_data(filename)
+    display_champions_count(champion_to_count)
+    print()
+    display_champion_country(countries)
 
-def load_champion_count(filename):
-    """Load Wimbledon champion data from a CSV file and count the number of wins per champion."""
+def load_wimbledon_data(filename):
+    """Load Wimbledon champion data from a CSV file."""
     champion_to_count = {}
+    countries = set()
     with open(filename, "r", encoding="utf-8-sig") as in_file:
         reader = csv.reader(in_file)
         next(reader)
@@ -23,12 +26,20 @@ def load_champion_count(filename):
                 champion_to_count[name] += 1
             else:
                 champion_to_count[name] = 1
-    return champion_to_count
 
-def display_champion_name_and_count(champion_to_count):
+            country = record[1]
+            countries.add(country)
+    return champion_to_count, countries
+
+def display_champions_count(champion_to_count):
     """Display the champions and their win count, neatly aligned."""
     name_width = max((len(name) for name in champion_to_count))
+    print("Wimbledon Champions:")
     for name, count in champion_to_count.items():
         print(f"{name:{name_width}} {count}")
+
+def display_champion_country(countries):
+    """Display the countries of the champions in alphabetical order."""
+    print(f"These {len(countries)} countries have won Wimbledon:\n{", ".join(sorted(countries))}")
 
 main()
