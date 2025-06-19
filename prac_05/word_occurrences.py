f62778e5dcba2b4673e620e9e2e4e8184c945085
CA <angelina.celine@my.jcu.edu.au>
2025-06-19 14:03:08 +0800

    Refactor code for cleaner and aligned output.

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -2,15 +2,18 @@
 Word Occurrences
 Estimate: 20 minutes
 Actual:    minutes
+start 1:33
 """
 
 string_to_count = {}
 text = input("Text: ")
 strings = text.split()
 
+word_width = max((len(word) for word in strings))
+
 for string in strings:
     if string in string_to_count:
         string_to_count[string] += 1
     else:
         string_to_count[string] = 1
-    print(f"{string} : {string_to_count[string]}")
+    print(f"{string:{word_width}} : {string_to_count[string]}")
