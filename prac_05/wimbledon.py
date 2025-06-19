1c19890fabfa5103c42028d3563f4cd975520af5
CA <angelina.celine@my.jcu.edu.au>
2025-06-20 01:04:47 +0800

    Refactor code into function.

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -8,6 +8,11 @@ import csv
 
 def main ():
     filename = "wimbledon.csv"
+    champion_to_count = load_champion_count(filename)
+    display_champion_name_and_count(champion_to_count)
+
+def load_champion_count(filename):
+    """Load Wimbledon champion data from a CSV file and count the number of wins per champion."""
     champion_to_count = {}
     with open(filename, "r", encoding="utf-8-sig") as in_file:
         reader = csv.reader(in_file)
@@ -18,11 +23,12 @@ def main ():
                 champion_to_count[name] += 1
             else:
                 champion_to_count[name] = 1
+    return champion_to_count
 
+def display_champion_name_and_count(champion_to_count):
+    """Display the champions and their win count, neatly aligned."""
     name_width = max((len(name) for name in champion_to_count))
-
     for name, count in champion_to_count.items():
         print(f"{name:{name_width}} {count}")
-
 
 main()
