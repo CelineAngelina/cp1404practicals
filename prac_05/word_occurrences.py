16c673203c84145cd77f206d1edbc4ebff7ec539
CA <angelina.celine@my.jcu.edu.au>
2025-06-19 14:18:34 +0800

    Refactor code so the output is sorted in alphabetical order.

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,19 +1,20 @@
 """
 Word Occurrences
 Estimate: 20 minutes
-Actual:    minutes
-start 1:33
+Actual:   43 minutes
 """
 
 string_to_count = {}
 text = input("Text: ")
 strings = text.split()
 
-word_width = max((len(word) for word in strings))
-
 for string in strings:
     if string in string_to_count:
         string_to_count[string] += 1
     else:
         string_to_count[string] = 1
-    print(f"{string:{word_width}} : {string_to_count[string]}")
+
+word_width = max((len(word) for word in strings))
+
+for word in sorted(string_to_count):
+    print(f"{word:{word_width}} : {string_to_count[word]}")
