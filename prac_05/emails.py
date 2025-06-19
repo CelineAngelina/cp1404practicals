a5b346cc1c0b3fab5b158303fe3d9396677e33ce
CA <angelina.celine@my.jcu.edu.au>
2025-06-19 21:37:04 +0800

    Refactor code by adding function to extract name from the email address given.

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,29 +1,33 @@
 """
 Emails
 Estimate: 60 minutes
-Actual:    minutes
-start 2:30
-stop 3:28
+Actual:   73 minutes
 """
-
-email_to_name = {}
-email = input("Email: ")
-
-while email != "":
-    username = email.split("@")
-    name = username[0].split(".")
-    formatted_name = " ".join(name).title()
-
-    check_name = input(f"Is your name {formatted_name}? (Y/n) ").lower()
-
-    if check_name != "no" and check_name != "n":
-        email_to_name[email] = formatted_name
-    else:
-        amend_name = input("Name: ")
-        email_to_name[email] = amend_name
+def main():
+    """Prompt the user to enter email addresses, extract and confirm names, then display their name and email."""
+    email_to_name = {}
     email = input("Email: ")
 
-print()
-for email, name in email_to_name.items():
-    print(f"{name} ({email})")
+    while email != "":
+        formatted_name = extract_name_from_email(email)
 
+        check_name = input(f"Is your name {formatted_name}? (Y/n) ").lower()
+
+        if check_name != "no" and check_name != "n":
+            email_to_name[email] = formatted_name
+        else:
+            amend_name = input("Name: ").title()
+            email_to_name[email] = amend_name
+        email = input("Email: ")
+
+    print()
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+def extract_name_from_email(email):
+    """Extract and format user's name from the given email address."""
+    username = email.split("@")
+    name = username[0].split(".")
+    return " ".join(name).title()
+
+main()
