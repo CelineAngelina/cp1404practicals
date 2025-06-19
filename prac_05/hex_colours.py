efe1f1c84ee8a66f2e1a5e606ccbf13661771134
CA <angelina.celine@my.jcu.edu.au>
2025-06-19 12:56:07 +0800

    Create dictionary for colour to code.

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,4 @@
+COLOUR_TO_CODE = {"Baby Pink": "#f4c2c2", "Aureolin": "#fdee00", "Celadon": "#ace1af", "Dark Orchid": "#9932cc",
+                "French Sky Blue": "#77b5fe", "Imperial Red": "#ed2939", "Mango": "#fdbe02", "Persian Blue": "#1c39bb",
+                "Pumpkin": "#ff7518", "Rocket Metallic": "#8a7f80"}
+print(COLOUR_TO_CODE)
