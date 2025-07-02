d534aed0bef71d86b50ead3ad15d416226f6e94f
CA <angelina.celine@my.jcu.edu.au>
2025-07-03 01:53:52 +0800

    Added docstring and update on completion time.

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,18 +1,22 @@
 """
 programming language
 Estimate: 30 minutes
-Actual:   19 minutes
+Actual:   22 minutes
 """
 
 class ProgrammingLanguage:
+    """Represent a programming language."""
     def __init__(self, name="", typing="", reflection="", year=0):
+        """Initialise a Programming Language instance."""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def is_dynamic(self):
+        """Determine if the programming language is dynamically typed."""
         return self.typing == "Dynamic"
 
     def __str__(self):
+        """Return a formatted string with details about the programming language."""
         return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
