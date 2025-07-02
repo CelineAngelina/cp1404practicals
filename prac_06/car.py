880287de423ad3f9ab03adbb4ca85df6df7ef0ef
CA <angelina.celine@my.jcu.edu.au>
2025-07-02 19:28:04 +0800

    Refactor code by adding 'name' field

--- prac_06/car.py
+++ prac_06/car.py
@@ -1,11 +1,12 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, name="", fuel=0):
         """Initialise a Car instance.
 
         fuel: float, one unit of fuel drives one kilometre
         """
+        self.name = name
         self.fuel = fuel
         self._odometer = 0
 
@@ -28,4 +29,4 @@ class Car:
         return distance
 
     def __str__(self):
-        return f"Car, fuel={self.fuel}, odometer={self._odometer}"
+        return f"{self.name}, fuel={self.fuel}, odometer={self._odometer}"
