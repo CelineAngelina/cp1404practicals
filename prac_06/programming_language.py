7e3087eae816265df3ae7a33f2aa349f7e6d38f8
CA <angelina.celine@my.jcu.edu.au>
2025-07-03 01:30:28 +0800

    Added init and is_dynamic methods

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -5,3 +5,11 @@ Actual:    minutes
 """
 
 class ProgrammingLanguage:
+    def __init__(self, name="", typing="", reflection="", year=0):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        return self.typing == "Dynamic"
