0f0f9adc7f85c833a9a183bd0851d945ae5db33d
CA <angelina.celine@my.jcu.edu.au>
2025-07-09 18:35:03 +0800

    Added code that takes user enter and write to CSV file.

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,11 +1,24 @@
 from prac_07.guitar import Guitar
 
-
 def main():
     """"""
     guitars = []
     load_guitar_data(guitars)
+    with open("guitars.csv", 'a') as out_file:
+        new_guitar_name = input("Name: ").title()
+        while new_guitar_name != "":
+            new_guitar_year = input("Year: ")
+            while not new_guitar_year.isdigit():
+                print("Invalid year")
+                new_guitar_year = input("Year: ")
+            new_guitar_cost = float(input("Cost: $"))
+            new_guitar = Guitar(new_guitar_name, new_guitar_year, new_guitar_cost)
+            guitars.append(new_guitar)
+            out_file.write(f"{new_guitar.name},{new_guitar.year},{new_guitar.cost}\n")
+            new_guitar_name = input("Name: ").title()
+
     guitars.sort()
+    print()
     for guitar in guitars:
         print(guitar)
 
