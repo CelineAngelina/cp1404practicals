458d74f7f8dfd69cb6c52b5fc49350325cedc9d8
CA <angelina.celine@my.jcu.edu.au>
2025-07-09 18:46:38 +0800

    Refactor code to get_valid_year and write_new_guitar_data function.

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,22 +1,10 @@
 from prac_07.guitar import Guitar
 
 def main():
-    """"""
+    """Load existing guitars, allow addition of new guitars data and display all guitars sorted by year."""
     guitars = []
     load_guitar_data(guitars)
-    with open("guitars.csv", 'a') as out_file:
-        new_guitar_name = input("Name: ").title()
-        while new_guitar_name != "":
-            new_guitar_year = input("Year: ")
-            while not new_guitar_year.isdigit():
-                print("Invalid year")
-                new_guitar_year = input("Year: ")
-            new_guitar_cost = float(input("Cost: $"))
-            new_guitar = Guitar(new_guitar_name, new_guitar_year, new_guitar_cost)
-            guitars.append(new_guitar)
-            out_file.write(f"{new_guitar.name},{new_guitar.year},{new_guitar.cost}\n")
-            new_guitar_name = input("Name: ").title()
-
+    write_new_guitar_data(guitars)
     guitars.sort()
     print()
     for guitar in guitars:
@@ -24,12 +12,31 @@ def main():
 
 def load_guitar_data(guitars):
     """Read guitar data from CSV file."""
-    in_file = open('guitars.csv', 'r')
-    for line in in_file:
-        parts = line.strip().split(',')
-        guitar = Guitar(parts[0], parts[1], float(parts[2]))
-        guitars.append(guitar)
-    in_file.close()
+    with open('guitars.csv', 'r') as in_file:
+        for line in in_file:
+            parts = line.strip().split(',')
+            guitar = Guitar(parts[0], parts[1], float(parts[2]))
+            guitars.append(guitar)
+
+def get_valid_year():
+    """Get new guitar's year by ensuring it is integer."""
+    new_guitar_year = input("Year: ")
+    while not new_guitar_year.isdigit():
+        print("Invalid year")
+        new_guitar_year = input("Year: ")
+    return new_guitar_year
+
+def write_new_guitar_data(guitars):
+    """Write new guitars data to CSV file."""
+    with open("guitars.csv", 'a') as out_file:
+        new_guitar_name = input("Name: ").title()
+        while new_guitar_name != "":
+            new_guitar_year = get_valid_year()
+            new_guitar_cost = float(input("Cost: $"))
+            new_guitar = Guitar(new_guitar_name, new_guitar_year, new_guitar_cost)
+            guitars.append(new_guitar)
+            out_file.write(f"{new_guitar.name},{new_guitar.year},{new_guitar.cost}\n")
+            new_guitar_name = input("Name: ").title()
 
 
 main()
