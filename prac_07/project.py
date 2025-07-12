099f82a020bcc618a516f41a2f6d9e99ba88c827
CA <angelina.celine@my.jcu.edu.au>
2025-07-13 00:49:35 +0800

    Added less than to compare projects by the priority, tested.

--- prac_07/project.py
+++ prac_07/project.py
@@ -9,14 +9,23 @@ class Project:
         self.completion_percentage = completion_percentage
 
     def __str__(self):
-        """Return string representation of a ProgrammingLanguage."""
+        """"""
         return f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate:.2f}, completion: {self.completion_percentage}%"
+
+    def __lt__(self, other):
+        return self.priority < other.priority
 
 
 def run_test():
-    test1 = Project("Organise Pantry", "20/07/2022", 1, 25, 55)
+    project1 = Project("Build Car Park", "12/09/2021", 2, 600000, 95)
+    project2 = Project("Organise Pantry", "20/07/2022", 1, 25, 55)
+    project3 = Project("Record Music Video", "01/12/2022", 9, 250000, 0)
 
-    print(test1)
+    projects = [project1, project2, project3]
+
+    projects.sort()
+    for project in projects:
+        print(project)
 
 if __name__ == "__main__":
     run_test()
