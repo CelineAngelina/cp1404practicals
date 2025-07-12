e74934406cf73e44b50538e8e0f37b3d9a805b51
CA <angelina.celine@my.jcu.edu.au>
2025-07-13 00:08:05 +0800

    Implemented code if user choose L from menu, it will prompt for filename and load the projects.

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -5,19 +5,57 @@ Actual:
 start 11:09
 """
 
+from prac_07.project import Project
 
 MENU = "- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date\n- (A)dd new project\n- (U)pdate project\n- (Q)uit"
 FILENAME = "projects.txt"
 
 def main():
-    print("Welcome to Pythonic Project Management")
-    in_file = open(FILENAME, 'r')
-    in_file.readline()
     project_count = 0
-    for line in in_file:
-        project_count += 1
-    in_file.close()
-    print(f"Loaded {project_count} projects from {FILENAME}")
+    print("Welcome to Pythonic Project Management")
+    with open(FILENAME, 'r') as in_file:
+        in_file.readline()
+        for line in in_file:
+            project_count += 1
+        print(f"Loaded {project_count} projects from {FILENAME}")
     print(MENU)
+
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "L":
+            new_filename = input("Enter the filename: ")
+            with open(new_filename, 'r') as in_file:
+                in_file.readline()
+                for line in in_file:
+                    parts = line.strip().split('\t')
+                    name = parts[0]
+                    start_date = parts[1]
+                    priority = int(parts[2])
+                    cost_estimate = float(parts[3])
+                    percent_complete = int(parts[4])
+                    project = Project(name, start_date, priority, cost_estimate, percent_complete)
+                    print(project)
+            choice = input(">>> ").upper()
+
+        elif choice == "S":
+            break
+
+        elif choice == "D":
+            break
+
+        elif choice == "F":
+            break
+
+        elif choice == "A":
+            break
+
+        elif choice == "U":
+            break
+
+        else:
+            print("Invalid choice")
+            choice = input(">>> ").upper()
+
+    print("Thank you for using custom-built project management software.")
 
 main()
