40902915853fe65b42c02ed007e2f7b6be52aa0d
CA <angelina.celine@my.jcu.edu.au>
2025-07-16 19:08:24 +0800

    Implement code to load the KV file in Kivy app

---
+++ prac_08/box_layout_demo.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+class BoxLayoutKV(App):
+   def build(self):
+       self.root = Builder.load_file('box_layout_demo.kv')
+       return self.root
+
+
+BoxLayoutKV().run()
