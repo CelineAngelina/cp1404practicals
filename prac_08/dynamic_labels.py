91c90cad085f8aec4b9ca69e708e5843eb7611ff
CA <angelina.celine@my.jcu.edu.au>
2025-07-17 02:06:24 +0800

    Added docstrings to the functions and class

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -3,12 +3,14 @@ from kivy.lang import Builder
 from kivy.uix.label import Label
 
 class NamesApp(App):
-    """"""
+    """Main program - Kivy app to create dynamic labels for a list of names."""
     def __init__(self, **kwargs):
+        """Initialize the app with a predefined list of names."""
         super().__init__(**kwargs)
         self.names = ["Alex", "Celine", "Jonathan"]
 
     def build(self):
+        """Build the Kivy GUI and add dynamic labels to the layout."""
         self.title = "Dynamic Labels"
         self.root = Builder.load_file('dynamic_labels.kv')
         for name in self.names:
