2086cec436a8cb1a1be99c56eb0471594c020fbe
CA <angelina.celine@my.jcu.edu.au>
2025-07-17 01:34:28 +0800

    Update docstring format

--- prac_08/squaring.py
+++ prac_08/squaring.py
@@ -6,7 +6,7 @@ __author__ = 'Celine Angelina'
 
 
 class SquareNumberApp(App):
-    """ SquareNumberApp is a Kivy App for squaring a number """
+    """SquareNumberApp is a Kivy App for squaring a number."""
     def build(self):
         """ build the Kivy app from the kv file """
         Window.size = (200, 100)
@@ -15,7 +15,7 @@ class SquareNumberApp(App):
         return self.root
 
     def handle_calculate(self, value):
-        """ handle calculation (could be button press or other call), output result to label widget """
+        """Handle calculation (could be button press or other call), output result to label widget."""
         try:
             result = float(value) ** 2
             self.root.ids.output_label.text = str(result)
