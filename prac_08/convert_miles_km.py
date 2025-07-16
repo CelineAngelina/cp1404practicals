26e8fb82b44480d48908221a5d61e7367300b15f
CA <angelina.celine@my.jcu.edu.au>
2025-07-17 00:56:47 +0800

    Added functions to handle increment and decrement of input number

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,12 +1,15 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 
 class ConvertMilesToKm(App):
     """"""
+    message = StringProperty()
     def build(self):
         """Build the Kivy app from the kv file."""
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
+        self.message = "Type in the field & press Enter"
         return self.root
 
     def handle_calculate(self, value):
@@ -17,5 +20,18 @@ class ConvertMilesToKm(App):
         except ValueError:
             pass
 
+    def handle_update(self):
+        """Handle changes to the text input by updating the model from the view."""
+        self.message = self.root.ids.user_input.text
+
+    def handle_increment(self):
+        value = int(self.root.ids.input_number.text)
+        value += 1
+        self.root.ids.input_number.text = str(value)
+
+    def handle_decrement(self):
+        value = int(self.root.ids.input_number.text)
+        value -= 1
+        self.root.ids.input_number.text = str(value)
 
 ConvertMilesToKm().run()
