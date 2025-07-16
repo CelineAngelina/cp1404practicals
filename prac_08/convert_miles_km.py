24a074efee7add75f4a37116df4ed9ba2021b9e6
CA <angelina.celine@my.jcu.edu.au>
2025-07-17 01:20:07 +0800

    using StringProperty to automatically update output when user pressed up or down button

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -4,34 +4,36 @@ from kivy.properties import StringProperty
 
 class ConvertMilesToKm(App):
     """"""
-    message = StringProperty()
+    output_text = StringProperty()
     def build(self):
         """Build the Kivy app from the kv file."""
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
-        self.message = "Type in the field & press Enter"
+        self.output_text = ""
         return self.root
 
     def handle_calculate(self, value):
         """"""
         try:
             result = float(value) * 1.60934
-            self.root.ids.output_label.text = str(result)
+            self.output_text = str(result)
         except ValueError:
-            pass
+            self.output_text = "0.0"
 
     def handle_update(self):
         """Handle changes to the text input by updating the model from the view."""
-        self.message = self.root.ids.user_input.text
+        self.handle_calculate(self.root.ids.input_number.text)
 
     def handle_increment(self):
         value = int(self.root.ids.input_number.text)
         value += 1
         self.root.ids.input_number.text = str(value)
+        self.handle_calculate(value)
 
     def handle_decrement(self):
         value = int(self.root.ids.input_number.text)
         value -= 1
         self.root.ids.input_number.text = str(value)
+        self.handle_calculate(value)
 
 ConvertMilesToKm().run()
