1f43223ca6558b97edd9bcc36fa0436532b37655
CA <angelina.celine@my.jcu.edu.au>
2025-07-21 15:54:56 +0800

    update on details

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -2,8 +2,8 @@ from taxi import Taxi
 
 my_taxi = Taxi("Prius 1", 100, 1.23)
 my_taxi.drive(40)
-print(f"{my_taxi}, current fare: ${my_taxi.get_fare()}")
+print(my_taxi)
 
 my_taxi.start_fare()
 my_taxi.drive(100)
-print(f"{my_taxi}, current fare: ${my_taxi.get_fare()}")
+print(my_taxi)
