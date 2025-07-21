efa48820be9156b8de135352ef07dc5b91b075d1
CA <angelina.celine@my.jcu.edu.au>
2025-07-21 15:53:26 +0800

    restart the meter, drove 100km, and print the updated details.

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -2,4 +2,8 @@ from taxi import Taxi
 
 my_taxi = Taxi("Prius 1", 100, 1.23)
 my_taxi.drive(40)
-print(f"{my_taxi}")
+print(f"{my_taxi}, current fare: ${my_taxi.get_fare()}")
+
+my_taxi.start_fare()
+my_taxi.drive(100)
+print(f"{my_taxi}, current fare: ${my_taxi.get_fare()}")
