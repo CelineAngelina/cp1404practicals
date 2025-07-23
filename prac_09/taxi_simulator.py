ab30b571c72bdb6ab5a3667919d754fe46f67c31
CA <angelina.celine@my.jcu.edu.au>
2025-07-23 19:11:13 +0800

    Refactor code using functions as repeated code used.

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -2,41 +2,47 @@ from prac_09.taxi import Taxi
 from prac_09.silver_service_taxi import SilverServiceTaxi
 
 MENU = "q)uit, c)hoose taxi, d)rive"
-bill_to_date = 0
-current_taxi = None
+def main():
+    bill_to_date = 0
+    current_taxi = None
 
-print("Let's drive!")
-taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
-choice = input(">>> ").lower()
-while choice != "q":
-    if choice == "c":
-        print("Taxis available:")
-        for i, taxi in enumerate(taxis):
-            print(f"{i} - {taxi}")
-        taxi_choice = int(input("Choose taxi: "))
-        if taxi_choice >= len(taxis):
-            print("Invalid taxi choice")
-        else:
-            current_taxi = taxis[taxi_choice]
-
-    elif choice == "d":
-        if current_taxi:
-            distance = float(input("Drive how far? "))
-            current_taxi.drive(distance)
-            trip_fare = current_taxi.get_fare()
-            print(f"Your {current_taxi.name} trip cost you ${trip_fare:,.2f}")
-            bill_to_date += trip_fare
-        else:
-            print("You need to choose a taxi before you can drive")
-
-    else:
-        print("Invalid option")
-
-    print(f"Bill to date: ${bill_to_date:,.2f}")
-    print(MENU)
+    print("Let's drive!")
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     choice = input(">>> ").lower()
+    while choice != "q":
+        if choice == "c":
+            print("Taxis available:")
+            display_taxi(taxis)
+            taxi_choice = int(input("Choose taxi: "))
+            if taxi_choice >= len(taxis):
+                print("Invalid taxi choice")
+            else:
+                current_taxi = taxis[taxi_choice]
 
-print(f"Total trip cost: ${bill_to_date}")
-print("Taxis are now:")
-for i, taxi in enumerate(taxis):
-    print(f"{i} - {taxi}")
+        elif choice == "d":
+            if current_taxi:
+                distance = float(input("Drive how far? "))
+                current_taxi.drive(distance)
+                trip_fare = current_taxi.get_fare()
+                print(f"Your {current_taxi.name} trip cost you ${trip_fare:,.2f}")
+                bill_to_date += trip_fare
+            else:
+                print("You need to choose a taxi before you can drive")
+
+        else:
+            print("Invalid option")
+
+        print(f"Bill to date: ${bill_to_date:,.2f}")
+        print(MENU)
+        choice = input(">>> ").lower()
+
+    print(f"Total trip cost: ${bill_to_date}")
+    print("Taxis are now:")
+    display_taxi(taxis)
+
+def display_taxi(taxis):
+    for i, taxi in enumerate(taxis):
+        print(f"{i} - {taxi}")
+
+
+main()
