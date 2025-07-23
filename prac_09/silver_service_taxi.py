98f971de5f624e8cc3595f6888419d2c810081f1
CA <angelina.celine@my.jcu.edu.au>
2025-07-23 17:37:22 +0800

    override get_fare to calculate the fare

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -8,3 +8,7 @@ class SilverServiceTaxi(Taxi):
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km = Taxi.price_per_km * fanciness
+
+    def get_fare(self):
+        """"""
+        return super().get_fare() + self.flagfall
