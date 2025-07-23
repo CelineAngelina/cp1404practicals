f2cf1e1e2b5b3326444c5f855a7dea7fafcb628a
CA <angelina.celine@my.jcu.edu.au>
2025-07-23 17:47:15 +0800

    Added __str__ method

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -5,6 +5,7 @@ class SilverServiceTaxi(Taxi):
     flagfall = 4.50
 
     def __init__(self, name, fuel, fanciness = float):
+        """"""
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km = Taxi.price_per_km * fanciness
@@ -12,3 +13,7 @@ class SilverServiceTaxi(Taxi):
     def get_fare(self):
         """"""
         return super().get_fare() + self.flagfall
+
+    def __str__(self):
+        """"""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall}"
