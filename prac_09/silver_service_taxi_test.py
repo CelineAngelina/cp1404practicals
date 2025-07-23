35501798fe945bc7e6b1d740dbd63e3e091ee453
CA <angelina.celine@my.jcu.edu.au>
2025-07-23 18:06:31 +0800

    Added assert test

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -2,5 +2,9 @@ from prac_09.silver_service_taxi import SilverServiceTaxi
 
 silver_taxi1 = SilverServiceTaxi("Hummer", 200, 2)
 silver_taxi1.drive(18)
+
+fare = silver_taxi1.get_fare()
+assert abs(fare) == 48.78, f"Expected $48.78, but got ${fare:.2f}"
+
 print(silver_taxi1)
-print(f"${silver_taxi1.get_fare()}")
+print(f"${fare}")
