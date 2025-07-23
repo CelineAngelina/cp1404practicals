4ddf4e9e17c4fa6f49b4dce6dcb3cc052aff6711
CA <angelina.celine@my.jcu.edu.au>
2025-07-23 19:26:03 +0800

    Added current_taxi.start_fare() to reset meter and implement error checking for inputs

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -2,30 +2,36 @@ from prac_09.taxi import Taxi
 from prac_09.silver_service_taxi import SilverServiceTaxi
 
 MENU = "q)uit, c)hoose taxi, d)rive"
+
 def main():
     bill_to_date = 0
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     current_taxi = None
 
     print("Let's drive!")
-    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+
     choice = input(">>> ").lower()
     while choice != "q":
         if choice == "c":
             print("Taxis available:")
             display_taxi(taxis)
             taxi_choice = int(input("Choose taxi: "))
-            if taxi_choice >= len(taxis):
-                print("Invalid taxi choice")
-            else:
+            try:
                 current_taxi = taxis[taxi_choice]
+            except IndexError:
+                print("Invalid taxi choice")
 
         elif choice == "d":
             if current_taxi:
-                distance = float(input("Drive how far? "))
-                current_taxi.drive(distance)
-                trip_fare = current_taxi.get_fare()
-                print(f"Your {current_taxi.name} trip cost you ${trip_fare:,.2f}")
-                bill_to_date += trip_fare
+                try:
+                    distance = float(input("Drive how far? "))
+                    current_taxi.start_fare()
+                    current_taxi.drive(distance)
+                    trip_fare = current_taxi.get_fare()
+                    print(f"Your {current_taxi.name} trip cost you ${trip_fare:,.2f}")
+                    bill_to_date += trip_fare
+                except ValueError:
+                    print("Enter valid number.")
             else:
                 print("You need to choose a taxi before you can drive")
 
@@ -36,7 +42,7 @@ def main():
         print(MENU)
         choice = input(">>> ").lower()
 
-    print(f"Total trip cost: ${bill_to_date}")
+    print(f"Total trip cost: ${bill_to_date:,.2f}")
     print("Taxis are now:")
     display_taxi(taxis)
 
