4705fca4ffb3f824f83b1fc98853b44b24867cfd
CA <angelina.celine@my.jcu.edu.au>
2025-07-23 18:36:26 +0800

    Implement code if user choose 'c' from the menu.

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -1,9 +1,19 @@
+from prac_09.taxi import Taxi
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
 print("Let's drive!")
 MENU = "q)uit, c)hoose taxi, d)rive"
+taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
 choice = input(">>> ").lower()
 while choice != "":
     if choice == "c":
-        pass
+        print("Taxis available:")
+        for i, taxi in enumerate(taxis):
+            print(f"{i} - {taxi}")
+        taxi_choice = int(input("Choose taxi: "))
+        if taxi_choice >= len(taxis):
+            print("Invalid taxi choice")
+
     elif choice == "d":
         pass
     elif choice == "q":
