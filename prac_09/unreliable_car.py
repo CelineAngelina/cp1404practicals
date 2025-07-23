296b664c8ee620e4f99352aaef5e44a87753dbd2
CA <angelina.celine@my.jcu.edu.au>
2025-07-23 17:21:59 +0800

    Added docstring

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -2,13 +2,14 @@ import random
 from prac_09.car import Car
 
 class UnreliableCar(Car):
-    """"""
+    """Specialised version of a UnreliableCar, depending on reliability."""
     def __init__(self, name, fuel, reliability):
-        """"""
+        """Initialise an UnreliableCar with name, fuel, and reliability."""
         super().__init__(name, fuel)
         self.reliability = reliability
 
     def drive(self, distance):
+        """Attempt to drive based on reliability."""
         random_number = random.randint(0,100)
         if random_number >= self.reliability:
             return 0
