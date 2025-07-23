e43587f89a2a24a170884c6cd1341fe97c2ab7f4
CA <angelina.celine@my.jcu.edu.au>
2025-07-23 18:26:22 +0800

    Implement basic outline of the program

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,14 @@
+print("Let's drive!")
+MENU = "q)uit, c)hoose taxi, d)rive"
+choice = input(">>> ").lower()
+while choice != "":
+    if choice == "c":
+        pass
+    elif choice == "d":
+        pass
+    elif choice == "q":
+        pass
+    else:
+        print("Invalid option")
+    print(MENU)
+    choice = input(">>> ").lower()
