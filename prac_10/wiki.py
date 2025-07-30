d8af7070874b3b17c67668da8e58253049dfb863
CA <angelina.celine@my.jcu.edu.au>
2025-07-31 00:45:05 +0800

    Added try-except and search function

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,7 +1,15 @@
+import wikipedia
 from wikipedia import search
+
+from wikipedia import DisambiguationError
 
 search_title = input("Enter page title: ")
 while search_title != "":
-    print(search(search_title))
+    try:
+        page = wikipedia.page(search_title, auto_suggest =False)
+        print(page.title)
+        print(search(search_title))
+    except DisambiguationError:
+        print("We need a more specific title. Try one of the following, or a new search:")
     search_title = input("Enter page title: ")
 print("Thank you.")
