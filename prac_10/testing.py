a9bc7eda6be9538961438024c1353c2816d300c3
CA <angelina.celine@my.jcu.edu.au>
2025-07-30 23:51:13 +0800

    uncomment the doctest

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -32,13 +32,13 @@ def run_tests():
     # the test below should fail
     assert repeat_string("hi", 2) == "hi hi"
 
+    # TODO: 1. fix the repeat_string function above so that it passes the failing test
+
     car = Car()
     assert car._odometer == 0, "Car does not set odometer correctly"
 
     # TODO: 2. write assert statements to show if Car sets the fuel correctly
-    # Note that Car's __init__ function sets the fuel in one of two ways:
-    # using the value passed in or the default
-    # You should test both of these
+
     car = Car(fuel=10)
     assert car.fuel == 10
 
@@ -49,7 +49,7 @@ run_tests()
 
 # TODO: 3. Uncomment the following line and run the doctests
 # (PyCharm may see your >>> doctest comments and run doctests anyway.)
-# doctest.testmod()
+doctest.testmod()
 
 # TODO: 4. Fix the failing is_long_word function
 # (Don't change the tests, change the function!)
